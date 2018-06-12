cdb6f2389c5701fb3500dee1d2470ebb47ec7e8a
divya007sen <divya25sen@gmail.com>
2018-06-12 10:18:07 +0530

    changes on 12 june

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
-// var parentDiv = document.createElement('div');
-//
-// parentDiv.className = "parentDiv";
-// document.body.appendChild(parentDiv);
-//
-// var size = "5px"
-//
-// for(let i = 0; i<2025 ; i++) {
-//     const div = document.createElement('div');
-//     div.id = "div" + i;
-//     div.innerHTML = "";
-//     div.style.width = size;
-//     div.style.height = size;
-//     div.style.border = "groove";
-//     div.style.background = "white";
-//     div.addEventListener('click', () => {
-//         div.style.backgroundColor = "red";
-//     })
-//     parentDiv.appendChild(div)
-// }
+let selectedColor = 'white';
+let cursorType = "default";
 
-
-for(var y = 0; y<20;y++)
-{
+for (var y = 0; y < 20; y++) {
     var row = document.createElement('div');
     row.className = 'row';
-    for(var x= 0;x<20; x++){
+    for (var x = 0; x < 20; x++) {
         const col = document.createElement('div');
         col.className = 'column';
-        col.addEventListener('click', () =>{
-           col.style.backgroundColor = 'red';
+        col.addEventListener('click', () => {
+            col.style.backgroundColor = selectedColor;
         });
         row.appendChild(col);
     }
     document.body.appendChild(row);
 }
+
+var parentDiv;
+var currentColorDiv = document.createElement('div');
+currentColorDiv.className = 'currentColorDiv';
+for (var y = 0; y < 2; y++) {
+    parentDiv = document.createElement('div');
+    parentDiv.className = 'parentDiv';
+    var length = 20;
+    if (y === 1) {
+        length = 7;
+    }
+    for (var x = 0; x < length; x++) {
+        const colorDiv = document.createElement('div');
+        colorDiv.className = 'colorDiv';
+        const hue = 'rgb(' + (Math.floor(Math.random() * 256))+ ',' + (Math.floor(Math.random() * 256)) + ',' +(Math.floor(Math.random() * 256)) + ')';
+        colorDiv.style.backgroundColor = hue;
+        colorDiv.addEventListener('click', () => {
+            selectedColor = hue;
+            const ele = document.getElementsByTagName('div');
+            for (let i = 0; i < ele.length; i++) {
+                ele[i].style.cursor = "hand";
+            }
+            currentColorDiv.style.backgroundColor = selectedColor;
+        });
+        parentDiv.appendChild(colorDiv);
+    }
+    document.body.appendChild(parentDiv);
+
+}
+var currentColorDivText = document.createElement('div');
+currentColorDivText.innerHTML = 'CURRENT COLOR > ';
+
+parentDiv.appendChild(currentColorDivText);
+parentDiv.appendChild(currentColorDiv);
 
 
 
